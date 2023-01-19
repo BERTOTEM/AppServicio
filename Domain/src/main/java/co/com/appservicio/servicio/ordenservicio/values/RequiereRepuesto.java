7fa7dd59a3a696c0f14f49e42b335ee5ec153a7a
KRod17 <keivys17@gmail.com>
2023-01-19 18:07:23 -0500

    Modificacion de values
    Se modifica value calificación

--- Domain/src/main/java/co/com/appservicio/servicio/ordenservicio/values/RequiereRepuesto.java
+++ Domain/src/main/java/co/com/appservicio/servicio/ordenservicio/values/RequiereRepuesto.java
@@ -4,15 +4,20 @@ import co.com.sofka.domain.generic.ValueObject;
 
 import java.util.Objects;
 
-public class RequiereRepuesto implements ValueObject<Boolean> {
+public  class RequiereRepuesto implements ValueObject<Descripcion> {
     private final Boolean value;
+    private final Descripcion descripcion;
 
-    public RequiereRepuesto(Boolean value){
+    public RequiereRepuesto(Boolean value, Descripcion descripcion){
         this.value = Objects.requireNonNull(value);
+        this.descripcion = descripcion;
     }
 
-    @Override
-    public Boolean value() {
-        return null;
+    public Descripcion value() {
+        if (!value.booleanValue()){
+            descripcion.value().descripcion("No usa");
+            descripcion.value().cantidad("0");
+        }
+        return descripcion;
     }
 }
