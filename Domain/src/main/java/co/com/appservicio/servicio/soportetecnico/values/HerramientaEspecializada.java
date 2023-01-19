fac79393618be43697f15f0e3be4b9bc966e5827
JULIAN MAZO <82735837+BERTOTEM@users.noreply.github.com>
2023-01-19 13:51:22 -0500

    Boolean implementacion

--- Domain/src/main/java/co/com/appservicio/servicio/soportetecnico/values/HerramientaEspecializada.java
+++ Domain/src/main/java/co/com/appservicio/servicio/soportetecnico/values/HerramientaEspecializada.java
@@ -2,7 +2,15 @@ package co.com.appservicio.servicio.soportetecnico.values;
 
 import co.com.sofka.domain.generic.ValueObject;
 
+import java.util.Objects;
+
 public class HerramientaEspecializada implements ValueObject<Boolean> {
+    private final Boolean value;
+
+    public HerramientaEspecializada(Boolean value) {
+        this.value = Objects.requireNonNull(value);
+    }
+
     @Override
     public Boolean value() {
         return null;
