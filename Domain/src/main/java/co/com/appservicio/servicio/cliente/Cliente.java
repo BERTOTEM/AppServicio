73a53b9fdb75aa97b33aa8aed0c28b6c33e74dd5
KRod17 <keivys17@gmail.com>
2023-01-18 23:30:37 -0500

    Punto de control, se crea ActualizarCLiente
    Se crea clase ActualizarCLiente y clase ClienteActualizado

--- Domain/src/main/java/co/com/appservicio/servicio/cliente/Cliente.java
+++ Domain/src/main/java/co/com/appservicio/servicio/cliente/Cliente.java
@@ -3,47 +3,58 @@ package co.com.appservicio.servicio.cliente;
 import co.com.appservicio.servicio.cliente.entities.Contacto;
 import co.com.appservicio.servicio.cliente.entities.Encuesta;
 import co.com.appservicio.servicio.cliente.events.ClienteCreado;
+import co.com.appservicio.servicio.cliente.events.ClienteActualizado;
 import co.com.appservicio.servicio.cliente.events.ContactoAgregado;
-import co.com.appservicio.servicio.cliente.events.EncuestaAgregado;
+import co.com.appservicio.servicio.cliente.events.EncuestaAgregada;
 import co.com.appservicio.servicio.cliente.values.*;
 import co.com.sofka.domain.generic.AggregateEvent;
 
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
-
-public class Cliente  extends AggregateEvent<ClienteId> {
+public class Cliente extends AggregateEvent<ClienteId> {
     protected InformacionCliente informacioncliente;
-    protected Encuesta encuesta;
-    protected Set<Contacto> contacto;
-    public Cliente(ClienteId clienteId,InformacionCliente informacioncliente) {
+    protected Encuesta encuestas;
+    protected Set<Contacto> contactos;
+
+    public Cliente(ClienteId clienteId, InformacionCliente informacioncliente) {
         super(clienteId);
         appendChange(new ClienteCreado(informacioncliente)).apply();
     }
 
-    public void AgregarEncuesta(EncuestaId encuestaId, Observaciones observaciones, Calificacion calificacion){
+    public void AgregarEncuesta(EncuestaId encuestaId, Observaciones observaciones, Calificacion calificacion) {
         Objects.requireNonNull(encuestaId);
         Objects.requireNonNull(observaciones);
         Objects.requireNonNull(calificacion);
-        appendChange(new EncuestaAgregado(encuestaId, observaciones, calificacion)).apply();
+        appendChange(new EncuestaAgregada(encuestaId, observaciones, calificacion)).apply();
     }
 
-    public void AgregarContacto(ContactoId contactoId, InformacionContacto informacioncontacto){
+    public void AgregarContacto(ContactoId contactoId, InformacionContacto informacioncontacto) {
         Objects.requireNonNull(contactoId);
         Objects.requireNonNull(informacioncontacto);
-
-        appendChange(new ContactoAgregado(contactoId,informacioncontacto)).apply();
+        appendChange(new ContactoAgregado(contactoId, informacioncontacto)).apply();
     }
 
-
-
-
+    public void ActualizarCliente(ClienteId clienteId, InformacionCliente informacionCliente) {
+        Objects.requireNonNull(clienteId);
+        Objects.requireNonNull(informacionCliente);
+        appendChange(new ClienteActualizado(clienteId, informacionCliente)).apply();
+    }
 
     public InformacionCliente getInformacioncliente() {
         return informacioncliente;
     }
 
-    public Encuesta getEncuesta() {
-        return encuesta;
+    public Encuesta getEncuestas() {
+        return encuestas;
     }
+
+    protected Optional<Contacto> getContactoPorID(ContactoId contactoId) {
+        return contactos
+                .stream()
+                .filter(contacto -> contacto.identity().equals(contactoId))
+                .findFirst();
+    }
+
 }
--- Domain/src/main/java/co/com/appservicio/servicio/cliente/events/EncuestaAgregada.java
+++ Domain/src/main/java/co/com/appservicio/servicio/cliente/events/EncuestaAgregada.java
@@ -5,17 +5,17 @@ import co.com.appservicio.servicio.cliente.values.EncuestaId;
 import co.com.appservicio.servicio.cliente.values.Observaciones;
 import co.com.sofka.domain.generic.DomainEvent;
 
-public class EncuestaAgregado extends DomainEvent {
+public class EncuestaAgregada extends DomainEvent {
     private final EncuestaId encuestaId;
     private final Observaciones observaciones;
     private final Calificacion calificacion;
 
 
-    public EncuestaAgregado(EncuestaId encuestaId, Observaciones observaciones, Calificacion calificacion) {
-        super("co.com.appservicio.servicio.cliente.events.EncuestaAgregado");
-        this.encuestaId=encuestaId;
-        this.observaciones=observaciones;
-        this.calificacion=calificacion;
+    public EncuestaAgregada(EncuestaId encuestaId, Observaciones observaciones, Calificacion calificacion) {
+        super("co.com.appservicio.servicio.cliente.events.EncuestaAgregada");
+        this.encuestaId = encuestaId;
+        this.observaciones = observaciones;
+        this.calificacion = calificacion;
 
     }
 
