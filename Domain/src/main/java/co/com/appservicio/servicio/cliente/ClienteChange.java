6f28446e0268e09ce472825d009a264a31193b1e
KRod17 <keivys17@gmail.com>
2023-01-19 21:29:16 -0500

    Punto de control, se modifica ClienteChange

--- Domain/src/main/java/co/com/appservicio/servicio/cliente/ClienteChange.java
+++ Domain/src/main/java/co/com/appservicio/servicio/cliente/ClienteChange.java
@@ -1,12 +1,14 @@
 package co.com.appservicio.servicio.cliente;
 
-import co.com.appservicio.servicio.cliente.commands.ActualizarCliente;
 import co.com.appservicio.servicio.cliente.entities.Contacto;
 import co.com.appservicio.servicio.cliente.entities.Encuesta;
 import co.com.appservicio.servicio.cliente.events.ClienteActualizado;
 import co.com.appservicio.servicio.cliente.events.ClienteCreado;
 import co.com.appservicio.servicio.cliente.events.ContactoAgregado;
 import co.com.appservicio.servicio.cliente.events.EncuestaAgregada;
+import co.com.appservicio.servicio.cliente.values.Calificacion;
+import co.com.appservicio.servicio.cliente.values.EncuestaId;
+import co.com.appservicio.servicio.cliente.values.Observaciones;
 import co.com.sofka.domain.generic.EventChange;
 
 import java.util.HashSet;
@@ -17,6 +19,10 @@ public class ClienteChange  extends EventChange {
        apply((ClienteCreado event)->{
            cliente.informacioncliente=event.getInformacioncliente();
            cliente.contactos = new HashSet<>();
+           cliente.encuestas = new Encuesta(new EncuestaId(),
+                   new Observaciones(cliente.encuestas.getObservaciones().value().observacion(),
+                           cliente.encuestas.getObservaciones().value().fecha()),
+                           new Calificacion(cliente.encuestas.getCalificacion().value()));
 
        });
        apply((ContactoAgregado event)->{
